0b804ed7bf48e17aa3f54880dbe73e3ffcf07dce
Cristian Cioflan <cioflanc@iis.ee.ethz.ch>
2024-01-23 21:06:12 +0100

    Finalized code generation template

--- codegen.sh
+++ codegen.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 
-rm -rf /usr/scratch/wetterhorn/cioflanc/kws_on_gap9/tiny_denoiser/trainlib_example_dscnn/network_name/ # TODO: parametrize
+export PROJECTPATH=$1
+export PROJECTNAME=$2
+
+
+
+rm -rf $PROJECTPATH/$PROJECTNAME # TODO: parametrize
+mkdir $PROJECTPATH
 cd tools/TrainLib_Deployer
-python TrainLib_Deployer.py --project_name test_net
-
-
+python TrainLib_Deployer.py --project_path $PROJECTPATH --project_name $PROJECTNAME 
+cd $PROJECTPATH/$PROJECTNAME/utils/
+python GM.py
+cp initdefines.h ../
+cp iodata.c ../
+cp iodata.h ../
