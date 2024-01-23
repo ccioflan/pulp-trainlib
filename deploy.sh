dd70c3615144f10533b724d97953894ef513c445
Cristian Cioflan <cioflanc@iis.ee.ethz.ch>
2024-01-23 16:40:49 +0100

    Tested Trainlib-only deployment, currently failing. Documented the failures.

--- deploy.sh
+++ deploy.sh
@@ -37,8 +37,13 @@ export MFCC=$4
 # export PATH=/usr/pack/gcc-4.9.1-af/x86_64-rhe6-linux/bin:$PATH
 # export LD_LIBRARY_PATH=/usr/pack/gcc-4.9.1-af/x86_64-rhe6-linux/lib64/:$LD_LIBRARY_PATH
 # export LD_LIBRARY_PATH=/usr/pack/gcc-4.9.1-af/x86_64-rhe6-linux/lib/:$LD_LIBRARY_PATH
+
+# export PATH=/usr/pack/gcc-9.2.0-af/x86_64-rhe6-linux/bin:$PATH
+# export LD_LIBRARY_PATH=/usr/pack/gcc-9.2.0-af/x86_64-rhe6-linux/lib64/:$LD_LIBRARY_PATH
+# export LD_LIBRARY_PATH=/usr/pack/gcc-9.2.0-af/x86_64-rhe6-linux/lib/:$LD_LIBRARY_PATH
+
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/scratch/wetterhorn/cioflanc/miniconda3/pkgs/mpfr-4.0.2-hb69a4c5_1/lib/
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/scratch/wetterhorn/cioflanc/mlonmcu_exercise6/exercise6/local_libs/
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/scratch/wetterhorn/cioflanc/teaching/classes/mlonmcu/mlonmcu_exercise6/exercise6/local_libs/
 
 export CC=gcc-9.2.1
 export CXX=g++-9.2.1
@@ -49,7 +54,7 @@ then
   # Select target
   if [[ $PLATFORM == "gvsoc" ]]
   then
-    source /usr/scratch/wetterhorn/cioflanc/tools/pulp-sdk/configs/pulp-open.sh
+    source /usr/scratch/wetterhorn/cioflanc/tools/pulp-sdk/configs/pulp-open.sh # PULP-SDK
   elif [[ $PLATFORM == "fpga" ]]
   then
     source /usr/scratch/wetterhorn/cioflanc/tools/pulp_sdk_fpga/pulp-sdk/configs/pulp-open.sh
@@ -60,14 +65,28 @@ then
 else
   export GAP_RISCV_GCC_TOOLCHAIN=/usr/scratch/wetterhorn/cioflanc/tools/gap_riscv_toolchain/
   # Select target
-  # source /usr/scratch/wetterhorn/cioflanc/tools/gap_sdk/sourceme.sh
-  source /usr/scratch/wetterhorn/cioflanc/tools/gap_sdk_mar23/gap_sdk/sourceme.sh # newest
+  
+  # source /usr/scratch/wetterhorn/cioflanc/tools/gap_sdk/sourceme.sh # GAP8 - GAPUINO
+  # Error
+  # gvsoc_launcher_debug: /lib64/libstdc++.so.6: version `GLIBCXX_3.4.20' not found (required by /usr/scratch/wetterhorn/cioflanc/tools/gap_sdk/install/workstation/lib/libpulpvp-debug.so)
+  
+  # source /usr/scratch/wetterhorn/cioflanc/tools/gap_sdk_mar23/gap_sdk/sourceme.sh # GAP8 - GAPOC_B_V2
+  # Error:
+  # Launching training procedure...
+  # 6023176965: 400609: [/sys/board/chip/cluster/pe0/warning                              ] Invalid access (pc: 0x1c00399e, offset: 0x57575757, size: 0x1, is_write: 0)
+
+  source /usr/scratch/wetterhorn/cioflanc/tools/gap_sdk_private/sourceme.sh # GAP9
+  # Error:
+  # 2392121649: 72001: [/chip/cluster/pe8/warning                                            ] Invalid access (pc: 0x1c014844, offset: 0x57575757, size: 0x1, is_write: 0)
+  # make: *** [/usr/scratch/wetterhorn/cioflanc/tools/gap_sdk_private/rtos/pmsis/os/freeRTOS/vendors/gwt/rules/freeRTOS_rules.mk:491: run] Error 1
+
 fi
 
-cd /usr/scratch/wetterhorn/cioflanc/kws_on_gap9/tiny_denoiser/trainlib_example_dscnn/DSCNN/ # TODO: parametrize
+cd /usr/scratch/wetterhorn/cioflanc/kws_on_gap9/tiny_denoiser/trainlib_example_dscnn/DSCNNS/ # TODO: parametrize
 python utils/GM.py
 
-make clean get_golden all run
+make clean get_golden all run platform=$PLATFORM
+
 
 make clean all run platform=$PLATFORM CORE=8 # sample=$AUDIO_SAMPLE sdk=$SDK memory=$MEMORY platform=$PLATFORM mfcc=$MFCC CORE=8
 
