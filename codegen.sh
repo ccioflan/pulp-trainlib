9299fdd24cb65b7e9e6b2d1fbf77c1387c37c8f0
Cristian Cioflan <cioflanc@iis.ee.ethz.ch>
2024-01-24 23:31:49 +0100

    Added automatic reading of linear layers

--- codegen.sh
+++ codegen.sh
@@ -5,7 +5,7 @@ export PROJECTNAME=$2
 
 
 
-# rm -rf $PROJECTPATH/$PROJECTNAME # TODO: parametrize
+rm -rf $PROJECTPATH/$PROJECTNAME # TODO: parametrize
 mkdir $PROJECTPATH
 cd tools/TrainLib_Deployer
 python TrainLib_Deployer.py --project_path $PROJECTPATH --project_name $PROJECTNAME 
--- tools/TrainLib_Deployer/TrainLib_Deployer.py
+++ tools/TrainLib_Deployer/TrainLib_Deployer.py
@@ -143,25 +143,94 @@ READ_MODEL_ARCH = True                # NOT IMPLEMENTED!!
 BACKEND
 """
 
+layer_list = []
+in_ch_list = []
+out_ch_list = []
+hk_list = []
+wk_list = []
+hin_list = []
+win_list = []
+h_pad_list = []
+w_pad_list = []
+opt_mm_fw_list = []
+opt_mm_wg_list = []
+opt_mm_ig_list = []
+data_type_list = []
+data_layout_list = []
+
+
 # Call the DNN Reader and then the DNN Composer 
 if READ_MODEL_ARCH :
 
-    supported_layers = [ 'Conv', 'Gemm'] # construct the network as a combination of these layers
+    # # Manually define the list of the network (each layer in the list has its own properties in the relative index of each list)
+    # layer_list      = [ 'conv2d', 'ReLU', 'DW', 'PW', 'ReLU', 'linear' ]
+    # # Layer properties
+    # in_ch_list      = [ 1, 4, 4, 4, 4, 4*2*2 ]          # Linear: size of input vector
+    # out_ch_list     = [ 4, 4, 4, 4, 4, 2 ]            # Linear: size of output vector
+    # hk_list         = [ 3, 1, 3, 1, 1, 1 ]             # Linear: = 1
+    # wk_list         = [ 3, 1, 3, 1, 1, 1 ]             # Linear: = 1
+    # # Input activations' properties
+    # hin_list        = [ 6, 4, 4, 2, 2, 1 ]             # Linear: = 1
+    # win_list        = [ 6, 4, 4, 2, 2, 1 ]             # Linear: = 1
+    # # Convolutional strides
+    # h_str_list      = [ 1, 1, 1, 1, 1, 1 ]             # Only for conv2d, maxpool, avgpool
+    # w_str_list      = [ 1, 1, 1, 1, 1, 1 ]             # Only for conv2d, maxpool, avgpool
+    # # Padding (bilateral, adds the specified padding to both image sides)
+    # h_pad_list      = [ 0, 0, 0, 0, 0, 0 ]             # Only for conv2d, DW
+    # w_pad_list      = [ 0, 0, 0, 0, 0, 0 ]             # Only for conv2d, DW
+    # # Define the lists to call the optimized matmuls for each layer (see mm_manager_list.txt, mm_manager_list_fp16.txt or mm_manager function body)
+    # opt_mm_fw_list  = [ 0, 0, 0, 0, 0, 0 ]
+    # opt_mm_wg_list  = [ 0, 0, 0, 0, 0, 0 ]
+    # opt_mm_ig_list  = [ 0, 0, 0, 0, 0, 0 ]
+    # Data type list for layer-by-layer deployment (mixed precision)
+    # data_type_list   = ['FP32', 'FP32', 'FP32', 'FP16', 'FP16', 'FP16']
+    # Data layout list (CHW or HWC) 
+    # data_layout_list = ['CHW', 'CHW', 'CHW', 'CHW', 'CHW', 'CHW']   # TO DO
 
     onnx_model = onnx.load(args.model_path)
     onnx.checker.check_model(onnx_model)
     onnx_graph = onnx_model.graph
     m_onnx_graph = shape_inference.infer_shapes(onnx_model)
+    graph_offset = int(m_onnx_graph.graph.value_info[0].name)
+    graph_len = len(m_onnx_graph.graph.value_info)
+
     # print (m_onnx_graph)
-    # print (m_onnx_graph.graph.node[0])
-    # print (m_onnx_graph.graph.node[1].attribute)
+    # print (graph_offset)
+    # print (graph_len)
+
+    # print (m_onnx_graph.graph.value_info[0].type.tensor_type.shape.dim) # returns the activation sizes
+    # print (m_onnx_graph.graph.output[0].type.tensor_type.shape.dim) # returns the activation sizes
+    # print (m_onnx_graph.graph.input[0].type.tensor_type.shape.dim) # returns the activation sizes
+
     
     for onnx_node in m_onnx_graph.graph.node:
-        # print (onnx_node.attribute)
-        if (onnx_node.op_type == 'Conv'):
-            print (onnx_node.attribute)
-        # print (onnx_node)
 
+        print (onnx_node)
+        print (onnx_node.attribute)
+
+        print ("-------------------------")
+        if (onnx_node.op_type == 'Gemm'):
+            in_ch_list.append(m_onnx_graph.graph.value_info[int(onnx_node.input[0])-graph_offset].type.tensor_type.shape.dim[0].dim_value)
+            if (int(onnx_node.input[0])-graph_offset == graph_len-1):
+                out_ch_list.append(m_onnx_graph.graph.output[0].type.tensor_type.shape.dim[0].dim_value)
+            else:
+                out_ch_list.append(m_onnx_graph.graph.value_info[int(onnx_node.output[0])-graph_offset].type.tensor_type.shape.dim[0].dim_value)
+            layer_list.append('linear')
+            hk_list.append(1)
+            wk_list.append(1)
+            hin_list.append(1)
+            win_list.append(1)
+            h_pad_list.append(0)
+            w_pad_list.append(0)
+            opt_mm_fw_list.append(0)
+            opt_mm_wg_list.append(0)
+            opt_mm_ig_list.append(0)
+            # TODO: Read from file
+            data_type_list.append('FP32')
+            # TODO: Read from file
+            # Note that this also determines the read position for in_ch_list and out_ch_list
+            data_layout_list.append('CHW')
+            
     # for attribute in node_iterating.attribute:
     #         if attribute.name not in ['kernel_shape', 'dilations', 'group', 'strides', 'pads'] or self.name == "Pad":
     #             if bool(attribute.i):
@@ -193,12 +262,8 @@ if READ_MODEL_ARCH :
     #     for idx, node_output_name in enumerate(node.output):
     #         print(idx, node_output_name)
 
-    print ("-------")
-    print (len(onnx_graph.initializer[0].raw_data))
-
-
-
-
+    # print ("-------")
+    # print (len(onnx_graph.initializer[0].raw_data))
 
 
     # for node_iterating in (self.graph.graph.node):
@@ -216,6 +281,25 @@ if READ_MODEL_ARCH :
     #             self.DORY_Graph.append(new_node)
     #         else:
     #             sys.exit("DORY Frontend. Node not parsed.")
+
+
+    print("Automatically generating project at location "+proj_folder)
+
+    # Check if the network training fits L1
+    memocc = composer.DNN_Size_Checker(layer_list, in_ch_list, out_ch_list, hk_list, wk_list, hin_list, win_list, 
+                                h_str_list, w_str_list, h_pad_list, w_pad_list,
+                                data_type_list, L1_SIZE_BYTES)
+
+    print("DNN memory occupation: {} bytes of {} available L1 bytes ({}%).".format(memocc, L1_SIZE_BYTES, (memocc/L1_SIZE_BYTES)*100))
+
+    # Call DNN Composer on the user-provided graph
+    composer.DNN_Composer(proj_folder, project_name, 
+                            layer_list, in_ch_list, out_ch_list, hk_list, wk_list, 
+                            hin_list, win_list, h_str_list, w_str_list, h_pad_list, w_pad_list,
+                            epochs, batch_size, learning_rate, optimizer, loss_fn,
+                            NUM_CORES, data_type_list, opt_mm_fw_list, opt_mm_wg_list, opt_mm_ig_list)
+
+    print("PULP project generation successful!")
 
 
 else:
