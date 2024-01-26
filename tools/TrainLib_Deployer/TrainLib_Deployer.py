fd9cf72014dcbe7ecb6845c9a3698726c56dace5
Cristian Cioflan <cioflanc@iis.ee.ethz.ch>
2024-01-26 17:16:57 +0100

    Removed redundant comments

--- tools/TrainLib_Deployer/TrainLib_Deployer.py
+++ tools/TrainLib_Deployer/TrainLib_Deployer.py
@@ -165,31 +165,6 @@ data_list = []
 # Call the DNN Reader and then the DNN Composer 
 if READ_MODEL_ARCH :
 
-    # # Manually define the list of the network (each layer in the list has its own properties in the relative index of each list)
-    # layer_list      = [ 'conv2d', 'ReLU', 'DW', 'PW', 'ReLU', 'linear' ]
-    # # Layer properties
-    # in_ch_list      = [ 1, 4, 4, 4, 4, 4*2*2 ]          # Linear: size of input vector
-    # out_ch_list     = [ 4, 4, 4, 4, 4, 2 ]            # Linear: size of output vector
-    # hk_list         = [ 3, 1, 3, 1, 1, 1 ]             # Linear: = 1
-    # wk_list         = [ 3, 1, 3, 1, 1, 1 ]             # Linear: = 1
-    # # Input activations' properties
-    # hin_list        = [ 6, 4, 4, 2, 2, 1 ]             # Linear: = 1
-    # win_list        = [ 6, 4, 4, 2, 2, 1 ]             # Linear: = 1
-    # # Convolutional strides
-    # h_str_list      = [ 1, 1, 1, 1, 1, 1 ]             # Only for conv2d, maxpool, avgpool
-    # w_str_list      = [ 1, 1, 1, 1, 1, 1 ]             # Only for conv2d, maxpool, avgpool
-    # # Padding (bilateral, adds the specified padding to both image sides)
-    # h_pad_list      = [ 0, 0, 0, 0, 0, 0 ]             # Only for conv2d, DW
-    # w_pad_list      = [ 0, 0, 0, 0, 0, 0 ]             # Only for conv2d, DW
-    # # Define the lists to call the optimized matmuls for each layer (see mm_manager_list.txt, mm_manager_list_fp16.txt or mm_manager function body)
-    # opt_mm_fw_list  = [ 0, 0, 0, 0, 0, 0 ]
-    # opt_mm_wg_list  = [ 0, 0, 0, 0, 0, 0 ]
-    # opt_mm_ig_list  = [ 0, 0, 0, 0, 0, 0 ]
-    # Data type list for layer-by-layer deployment (mixed precision)
-    # data_type_list   = ['FP32', 'FP32', 'FP32', 'FP16', 'FP16', 'FP16']
-    # Data layout list (CHW or HWC) 
-    # data_layout_list = ['CHW', 'CHW', 'CHW', 'CHW', 'CHW', 'CHW']   # TO DO
-
     onnx_model = onnx.load(args.model_path)
     onnx.checker.check_model(onnx_model)
     onnx_graph = onnx_model.graph
